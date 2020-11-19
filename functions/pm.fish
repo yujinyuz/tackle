6105d9688451e5fe31163f4a89339c860b608528
Eugene Oliveros <eevoliveros@gmail.com>
2020-11-19 11:17:20 +0800

    Pass arguments to python manage.py

--- functions/pm.fish
+++ functions/pm.fish
@@ -1,4 +1,4 @@
 function pm --description "Find and execute manage.py"
     # -quit to exit after first match
-    command find . -type f -name "manage.py" -exec python {} \; -quit
+    command find . -type f -name "manage.py" -exec python {} $argv \; -quit
 end
