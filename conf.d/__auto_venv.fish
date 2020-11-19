da65f16ac09775650661a34bdea184363d8f7781
Eugene Oliveros <eevoliveros@gmail.com>
2020-11-19 11:14:45 +0800

    Activate virtualenv on fish_prompot event

--- conf.d/__auto_venv.fish
+++ conf.d/__auto_venv.fish
@@ -1,4 +1,5 @@
-function __auto_venv --on-variable PWD --description "Automatically activate python venv"
+# https://fishshell.com/docs/current/cmds/function.html#cmd-function
+function __auto_venv --on-event fish_prompt --description "Automatically activate python venv"
   set -l venv_name (basename $PWD | tr . -)
 
   if test -d $HOME/.virtualenvs/$venv_name
