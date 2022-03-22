a63229280db150552328aa76a7007a7e1cc5f29b
Eugene Oliveros <eevoliveros@gmail.com>
2022-03-22 10:05:51 +0800

    feat: add scratch

---
+++ scratch.fish
@@ -0,0 +1,10 @@
+function scratch --description 'Create temp dir'
+  set -l tmpdir (mktemp -d)
+  set -l cwd $PWD
+  echo " $tmpdir"
+  cd "$tmpdir"; fish
+  echo "removing scratch dir: $tmpdir"
+  rm -fr "$tmpdir"
+  cd "$cwd"
+end
+
