6d760a3cf411649374d73985588f10d0c808e9cb
Eugene Oliveros <eevoliveros@gmail.com>
2022-03-22 10:06:40 +0800

    refactor: disable virtualenv commands
    Currently trying out virtualfish and it seems to cater my use case for
    now

--- functions/vactivate.fish
+++ functions/vactivate.fish
@@ -1,4 +1,4 @@
-function vactivate --argument-names 'venv_name' --description 'Create virtualenv named the same as current directory'
-  set -l venv_dir $HOME/.local/share/virtualenvs
-  source $venv_dir/$venv_name/bin/activate.fish
-end
+# function vactivate --argument-names 'venv_name' --description 'Create virtualenv named the same as current directory'
+#   set -l venv_dir $HOME/.local/share/virtualenvs
+#   source $venv_dir/$venv_name/bin/activate.fish
+# end
--- functions/venv.fish
+++ functions/venv.fish
@@ -1,27 +1,27 @@
-function venv --description 'Create virtualenv named the same as current directory' --argument-names python_version name
-  set -l python_bin
-  set -l venv_name
-  set -l venv_dir $HOME/.local/share/virtualenvs
-
-  if not test -n "$python_version"
-    # Use default python version set by asdf
-    set python_bin ($HOME/.asdf/bin/asdf which python)
-  else
-    set python_bin $ASDF_DIR/installs/python/$python_version/bin/python
-  end
-
-  if not test -n "$name"
-    set venv_name (basename $PWD | tr . -)
-  else
-    set venv_name $name
-  end
-
-  if not test -e $python_bin
-    echo "Python version `$python_version` is not installed."
-    return 1
-  end
-
-  echo Creating virtualenv $venv_name ...
-  $python_bin -m venv $venv_dir/$venv_name
-  source $venv_dir/$venv_name/bin/activate.fish
-end
+# function venv --description 'Create virtualenv named the same as current directory' --argument-names python_version name
+#   set -l python_bin
+#   set -l venv_name
+#   set -l venv_dir $HOME/.local/share/virtualenvs
+#
+#   if not test -n "$python_version"
+#     # Use default python version set by asdf
+#     set python_bin ($HOME/.asdf/bin/asdf which python)
+#   else
+#     set python_bin $ASDF_DIR/installs/python/$python_version/bin/python
+#   end
+#
+#   if not test -n "$name"
+#     set venv_name (basename $PWD | tr . -)
+#   else
+#     set venv_name $name
+#   end
+#
+#   if not test -e $python_bin
+#     echo "Python version `$python_version` is not installed."
+#     return 1
+#   end
+#
+#   echo Creating virtualenv $venv_name ...
+#   $python_bin -m venv $venv_dir/$venv_name
+#   source $venv_dir/$venv_name/bin/activate.fish
+# end
