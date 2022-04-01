5aca3efc5dece324228b0a1b61af47156590a07e
Eugene Oliveros <eevoliveros@gmail.com>
2022-04-01 17:44:46 +0800

    refactor: remove python related files in favor of virtualfish

--- functions/vactivate.fish
+++
@@ -1,4 +0,0 @@
-# function vactivate --argument-names 'venv_name' --description 'Create virtualenv named the same as current directory'
-#   set -l venv_dir $HOME/.local/share/virtualenvs
-#   source $venv_dir/$venv_name/bin/activate.fish
-# end
