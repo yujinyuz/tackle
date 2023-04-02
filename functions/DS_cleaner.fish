a47692978703806daa1eb20799aa5d53074a103b
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 18:26:54 +0800

    refactor: improve DS_cleaner

--- functions/DS_cleaner.fish
+++ functions/DS_cleaner.fish
@@ -1,3 +1,9 @@
 function DS_cleaner --description "Cleanup .DS_Store files"
-    find . -type f -name '.DS_Store' -delete -print
+    set confirm (read -P "Are you sure you want to delete all .DS_Store files? [y/n] ")
+    if test "$confirm" = y
+        find . -type f -name '.DS_Store' -depth -exec rm -f {} \;
+        echo ".DS_Store files deleted."
+    else
+        echo "Operation cancelled."
+    end
 end
