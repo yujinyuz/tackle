e46c2bd4b1b18153a062e58193e52ac05bca9816
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 18:26:36 +0800

    refactor: improve pycleanup

--- functions/pycleanup.fish
+++ functions/pycleanup.fish
@@ -1,3 +1,10 @@
 function pycleanup --description "Cleanup python byte-code and cache"
-    find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+    # NOTE: The -o in find stands for -or while -a stands for -and
+    set -l deleted_files (command find . -type f -name '*.py[co]' -delete -print -o -type d -name '__pycache__' -delete -print)
+    if test -n "$deleted_files"
+        echo "Deleted files:"
+        echo "$deleted_files"
+    else
+        echo "No files deleted."
+    end
 end
