8123aa0cd575ce3d217d96273c17aea495dfb8b4
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 17:59:49 +0800

    refactor: improve pm command

--- functions/pm.fish
+++ functions/pm.fish
@@ -1,4 +1,19 @@
 function pm --description "Find and execute manage.py"
-    # -quit to exit after first match
-    command find . -type f -name "manage.py" -exec python {} $argv \; -quit
+    # Use `command -v` to find the full path of the `python` command
+    set -l python_cmd (command -v python)
+
+    # Use `find`'s `-quit` option in a separate argument
+    set -l manage_path (command find . -type f -name "manage.py" -print -quit)
+
+    # Check if manage.py exists
+    if test -z "$manage_path"
+        echo "No manage.py found."
+        return 1
+    end
+
+    # Get available Django management commands using `manage.py`
+    # set commands ($python_cmd $manage_path shell -c "import sys; from django.core.management import execute_from_command_line; execute_from_command_line(['', 'help']); print('\n'.join(sys.argv[2:]))" 2>/dev/null)
+
+    # Run `manage.py` with provided arguments
+    $python_cmd $manage_path $argv
 end
