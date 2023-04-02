e2c0b73b2826e26a54d80e118fb1c623905f3abb
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 17:42:19 +0800

    feat: refactor killport command

--- functions/killport.fish
+++ functions/killport.fish
@@ -1,11 +1,32 @@
 # https://til.hashrocket.com/posts/e4c8c665a8-find-and-kill-all-processes-listening-on-a-port
 function killport --description "Kill process(es) with the given port"
-    set -l PIDS (lsof -ti tcp:$argv)
 
-    if test -n "$PIDS"
-        set -l PNAMES (ps $PIDS -o comm=)
-        kill $PIDS
-        echo "Successfully killed $PNAMES on port $argv"
+    if test -z "$argv"
+        echo "Please provide a port number as an argument"
+        return 1
     end
 
+
+    set port $argv[1]
+    set pid (lsof -i tcp:$port -t)
+
+
+    if test -z "$pid"
+        echo "No program is running on port $port"
+        return 1
+    end
+
+
+    echo "Program running on port $port:"
+    ps -p $pid -o comm=
+    echo "Do you want to kill this program? (y/n)"
+    read confirmation
+
+    if test "$confirmation" = y
+        kill -9 $pid
+        echo "Successfully killed program"
+        return 0
+    end
+
+    return 0
 end
