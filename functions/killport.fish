1506a29233261f1d08a768f0e8cd0dfbbbe34704
Eugene Oliveros <yujin@jinyuz.dev>
2023-04-02 17:33:21 +0800

    feat: display process name on killport

--- functions/killport.fish
+++ functions/killport.fish
@@ -1,11 +1,11 @@
 # https://til.hashrocket.com/posts/e4c8c665a8-find-and-kill-all-processes-listening-on-a-port
 function killport --description "Kill process(es) with the given port"
+    set -l PIDS (lsof -ti tcp:$argv)
 
-  set -l PIDS (lsof -ti tcp:$argv)
-
-  if test -n "$PIDS"
-    kill $PIDS
-    echo "Successfully killed processes on port $argv"
-  end
+    if test -n "$PIDS"
+        set -l PNAMES (ps $PIDS -o comm=)
+        kill $PIDS
+        echo "Successfully killed $PNAMES on port $argv"
+    end
 
 end
